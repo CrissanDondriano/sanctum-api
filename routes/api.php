cf0a10bfcc1aa2ddf5e09f5d62771921e05c0a77
Crissan Dondriano <dondrianocrissan20@gmail.com>
2024-05-19 22:32:46 +0800

    adding authentication for post and comments

--- routes/api.php
+++ routes/api.php
@@ -10,13 +10,16 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 
-Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
-    Route::resource('posts', PostController::class);
-    Route::resource('comments', CommentController::class);
-});
-
 Route::middleware('auth:sanctum')->group(function () {
-    Route::resource('comments', CommentController::class)->except(['create', 'edit']);
+    Route::get('/posts', [PostController::class, 'index']);
+    Route::post('/posts', [PostController::class, 'store'])->middleware('role:admin,user');
+    Route::get('/posts/{post}', [PostController::class, 'show']);
+    Route::put('/posts/{post}', [PostController::class, 'update'])->middleware('role:admin,user');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->middleware('role:admin');
+
+    Route::get('/comments', [CommentController::class, 'index']);
+    Route::post('/comments', [CommentController::class, 'store'])->middleware('role:admin,user');
+    Route::get('/comments/{comment}', [CommentController::class, 'show']);
+    Route::put('/comments/{comment}', [CommentController::class, 'update'])->middleware('role:admin,user');
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->middleware('role:admin');
 });
-
-
