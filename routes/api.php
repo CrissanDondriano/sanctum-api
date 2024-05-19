c19315cc1524fa1217b2bb3115cc861a8b19216a
Crissan Dondriano <dondrianocrissan20@gmail.com>
2024-05-19 21:03:31 +0800

    Make model and controller for post and comment

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 
-
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::resource('posts', PostController::class);
+    Route::resource('comments', CommentController::class);
+});
 
 
